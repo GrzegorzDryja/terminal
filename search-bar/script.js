6713f5376fef807f77e794a874733facd2e0f2b1
Grzegorz Dryja <grzegorz.dryja@agentify.tech>
2024-02-08 11:46:33 +0100

    fix: handle delay and empty response, refactor

--- search-bar/script.js
+++ search-bar/script.js
@@ -1,47 +1,57 @@
 'use strict';
 
 const searchBar = document.getElementById('search-bar');
-const searchSuggestions = document.getElementById('search-suggestions');
+const suggestions = document.getElementById('suggestions');
 const loader = document.getElementById('loader');
 
 let loading = false;
 let searchRequest = '';
 let searchResponse = [];
-let timeOut;
+let fetchSuggestionsTimeout;
 
 searchBar.addEventListener('input', (event) => {
   searchRequest = event.target.value;
-
-  if (searchRequest.length < 3 || loading) {
-    loader.setAttribute('class', 'loader loader--enabled');
-    clearTimeout(timeOut);
+  clearTimeout(fetchSuggestionsTimeout);  
+  
+  if (searchRequest.length <= 2) {
+    clearSearchSuggestions();
     return;
   }
-
-  //Zrób tak, żeby jak jest wysłany event, to żeby poczekał jakoś na kolejne dopisywania
-  //A no i, żeby nie wysyłał jak cały czas jest coś wpisywane, delay
-
-  timeOut = setTimeout(async () => {
-    loading = true;
-    // searchSuggestions.querySelectorAll('*').forEach(childNode => childNode.remove());
-
-    const response = await fetch(` https://dummyjson.com/products/search?q=${searchRequest}&limit=5&delay=1000`);
-    const data = await response.json();
-
-    if (data) {
-      loading = false;
-      loader.setAttribute('class', 'loader loader--disabled');
-    }
-    if (data.products.length === 0) return;
-
-    //Dodaj obsługę braku sugestii
-    searchResponse = data.products;
-    searchSuggestions.querySelectorAll('p').forEach(n => n.remove()); //To musi też zniknąć jak nie nic w wyszukiwaniu, można pokazywać tylko jak jest focus na input
-
-    searchResponse.forEach((product) => {
-      const p = document.createElement('p');
-      p.innerText = `${product.title} | ${product.price}`;
-      searchSuggestions.appendChild(p);
-    });
-  }, 500);
+  
+  fetchSuggestionsTimeout = setTimeout(fetchSuggestions, 500);
 });
+
+const clearSearchSuggestions = () => {
+  suggestions.setAttribute('class', 'suggestions suggestions--disabled');
+  suggestions.querySelectorAll('p').forEach((p) => p.remove());
+};
+
+const fetchSuggestions = async () => {
+  loading = true;
+  loader.setAttribute('class', 'loader loader--enabled');
+
+  const response = await fetch(` https://dummyjson.com/products/search?q=${searchRequest}&limit=5&delay=1000`);
+  const data = await response.json();
+
+  if (data) {
+    loading = false;
+    searchResponse = data.products;
+    loader.setAttribute('class', 'loader loader--disabled');
+    clearSearchSuggestions();
+    showSuggestions();
+  }
+
+  if (data.products.length === 0) {
+    searchResponse = [{ title: 'Nie znalazłem sugestii... Spróbuj wpisać inne hasło :)', price: 'To akurat nic nie kosztuje' }]
+    showSuggestions()
+  }
+};
+
+const showSuggestions = () => {
+  suggestions.setAttribute('class', 'suggestions suggestions--enabled');
+  searchResponse.forEach((product) => {
+    const p = document.createElement('p');
+    p.innerText = `${product.title} | ${product.price}`;
+    suggestions.appendChild(p);
+  });
+};
